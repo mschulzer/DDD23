3d9e157c261b215aad86fcca5b846c95aa4f347a
Morten Schultz <45948423+mschulzer@users.noreply.github.com>
2023-09-08 11:03:57 +0200

    Create algoritmer.js

---
+++ algoritmer.js
@@ -0,0 +1,27 @@
+
+
+/******************************************************
+ *    ALGORITME # 1
+ ******************************************************/
+function findGGMotiver(inputStreng) {
+    let frekvenser = {
+        'GGA': 0,
+        'GGC': 0,
+        'GGG': 0,
+        'GGT': 0
+    };
+
+    // Løb strengen igennem og find eventuelle GG* mønstre
+    for (let i = 0; i < inputStreng.length - 2; i++) {
+        let moenster = inputStreng.substring(i, i+3); // Udtag en understreng af 3 nukleotider
+        if (moenster.startsWith('GG') && frekvenser.hasOwnProperty(moenster)) {
+            frekvenser[moenster]++;
+        }
+    }
+
+    return frekvenser;
+}
+
+let dnaStump = "AGTCCAGTTTAAGTCTGCCGTTTGCTGACAGGACGGCATTGGCTGGGTCGCGGGCAACGTGGTGTCTAC";
+
+console.log(findGGMotiver(dnaStump));
